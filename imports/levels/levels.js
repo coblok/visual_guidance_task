7a86f4a67421b6e9a3a47c9c82ea3d49afd84630
Heikkinen <pyryheik@ad.helsinki.fi>
2022-02-03 13:46:07 +0200

    Update levels.js
    new blocks

--- imports/levels/levels.js
+++ imports/levels/levels.js
@@ -2,66 +2,58 @@
 export var levels = [
     //1 piece
     "hBPrMiRDzzusuAKfc",
-    //2 pieces
-    "4FdwcBRqwcYPLnRaL",
+    //2
+    "MS9MqDSgYdvwJhbzg",
     "L3tK8RrfJ9NJHjfyB",
-//	"cz7CF9tNG63ZdHaEc",
-    //3 pieces
-	"DSfvRhCCDJNmg7cp7",
+    //3
+    "9GyqYyrx2nwQvWAjo",
     "hTBDzb5GzRd7YmjAt",
-//	"9GyqYyrx2nwQvWAjo", //not in original
-//	"mHojzWurJkMNh6H2m", //not in original
-//	"CZhjN3Mg83sDXZxab", //not in original
-//	"CnALiDbrTHRBQftzM", //not in original
-    //4 pieces
-	"NX7TBK9nPDaFPqPF9",
-	"5DjaNXgQMCQJyg4bx",
-	"mWc5mviyswQue4NJy",
-	"yQYb3Kvi2j24hMp5E",
-    //5 pieces
-    "vC3xB7akPmo4aa6oT",
-	"7Km6kK6o2Pcd2mZwf",
-	"GY3JZyrXjChDuRgPG",
-	"BANQCFutBGWwS9HW8", // Super easy
-	"drX4f5fenhwAgLtbc", //Super easy
-	"pDirDNcGmGWNTyvEW",
-	"bK9d2tYpuJaZmqTTR",
-	"Es5MYocKRMqLj5H3W",
-	"Qi8wBF5axRdzmbMX9",
-	"uGPhSpToLYG8PiNiX"
-  ];
+    "NX7TBK9nPDaFPqPF9",
+    "5DjaNXgQMCQJyg4bx",
+    "s4zurnv9fsFKzBrJP",
+    "yQYb3Kvi2j24hMp5E",
+    //4
+    "drX4f5fenhwAgLtbc",
+    "7Km6kK6o2Pcd2mZwf",
+    "ziJCMjLCpFvZ3eSyi",
+    "HECXJgpujkMsefwfd",
+    //5
+    "pDirDNcGmGWNTyvEW",
+    "2tN6nTA5ecAECDP32",
+    "Es5MYocKRMqLj5H3W",
+    "waPbYYPb8EGk9vgxj",
+    "B35LZ2bRWatYdtGrx",
+    //6
+    "nu6FPopuqww9iv6iS"
+];
   
 
   export var levels_2 = [
-    // 1 piece
-	"QAC2arQpezgJRkJb9",
-	//2 pieces
-    "MS9MqDSgYdvwJhbzg",
-    "Q6WquekFpAb7G34sB",
-    //3 pieces
-//	"DSfvRhCCDJNmg7cp7",
-//    "hTBDzb5GzRd7YmjAt",
-	"9GyqYyrx2nwQvWAjo",
-//	"mHojzWurJkMNh6H2m",
-//	"CZhjN3Mg83sDXZxab",
-	"CnALiDbrTHRBQftzM",
-    //4 pieces
-	"mSk2bJxMpBCBQ5sNH",
-	"pSQtnXuvdYuzXyede",
-	"s4zurnv9fsFKzBrJP", 
-	"cWfDoXk7wNDgz8hqi",
-    //5 pieces
-    "ziJCMjLCpFvZ3eSyi",
-	"HECXJgpujkMsefwfd",
-	"EgbDFAipWZ3GP7k4X", 
-	"2tN6nTA5ecAECDP32",
-	"BzoJMSH34ZcasrAXJ",
-	"waPbYYPb8EGk9vgxj",
-	"gjN2YYLqvE8Sy9JHS",
-	"B35LZ2bRWatYdtGrx",
-	//6 pieces
-	"nu6FPopuqww9iv6iS",
-	"TJKd3WoYjBQNLgKXt",
+      //1
+      "QAC2arQpezgJRkJb9",
+      //2
+      "4FdwcBRqwcYPLnRaL",
+      "Q6WquekFpAb7G34sB",
+      //3
+      "DSfvRhCCDJNmg7cp7",
+      "CnALiDbrTHRBQftzM",
+      //4
+      "mSk2bJxMpBCBQ5sNH",
+      "pSQtnXuvdYuzXyede",
+      "mWc5mviyswQue4NJy",
+      "cWfDoXk7wNDgz8hqi",
+      //5
+      "BANQCFutBGWwS9HW8",
+      "GY3JZyrXjChDuRgPG",
+      "vC3xB7akPmo4aa6oT",
+      "EgbDFAipWZ3GP7k4X",
+      "bK9d2tYpuJaZmqTTR",
+      "BzoJMSH34ZcasrAXJ",
+      "Qi8wBF5axRdzmbMX9",
+      "gjN2YYLqvE8Sy9JHS",
+      "uGPhSpToLYG8PiNiX",
+      //6
+      "TJKd3WoYjBQNLgKXt"
   ];
 
 
